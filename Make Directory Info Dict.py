88a7536d0314bf22c7b42f53582ceb6d67223793
g33kyM0m <g33kyM0m@users.noreply.github.com>
2018-04-10 21:10:47 -0500

    Update Make Directory Info Dict.py
    Added comments, etc

--- Make Directory Info Dict.py
+++ Make Directory Info Dict.py
@@ -1,22 +1,29 @@
-# Create a json dictionary file in current directory with its name, path, and 'parent' directories. Useful to streamline current and future Editorial & Pythonista projects.
+# Create a json dictionary file in current directory with its name, path, and 'parent' directories. 
+# Useful to streamline current and future Editorial & Pythonista projects.
 
 # by g33kym0m - www.g33kymom.com
 # from https://github.com/g33kyM0m/iOS-Editorial-Pythonista-Tools
 
-import os, json
+import os, json, editor
 
 i = 1
 neg = -2
+jn = '/'
 
-here = os.path.abspath(os.curdir)
-dirList = here.split('/')
+# GET CURRENT DIRECTORY NAME AND PATH USING CURRENT DOC
+pth = editor.get_path()
+dirList = pth.split(jn)
+here = jn.join(dirList[0:-1])
+dirList = here.split(jn)
 dirName = dirList[-1]
 
+# GET LOCATION OF MAIN (DOCUMENTS) DIRECTORY IN TREE
 dirFind = 'Documents'
 findLoc = dirList.index(dirFind)
 allCnt = len(dirList)
 parCnt = allCnt - findLoc
 
+# CREATE PARENT DIRECTORY LIST
 parents = {}
 
 while i < parCnt:
@@ -30,8 +37,7 @@ final = {'name': dirName, 'location': here, 'parents': parents}
 
 finalDict = json.dumps(final)
 
-
-
+# CREATE & SAVE DATA DICTIONARY FILE
 dName = 'dir_info.json'
 dFile = open(dName, 'w')
 dFile.write(finalDict)
